fbcfb851ee6c70e6b9bc38e3e3b83f8d185e913f
shelockid@gmail.com <shelockid@gmail.com>
2014-01-23 14:18:31 +0800

    增加自定义函数，取消^M标志

--- Linux/tool/emacs/.emacs.d/config/base-config.el
+++ Linux/tool/emacs/.emacs.d/config/base-config.el
@@ -17,3 +17,10 @@
 
 ;;隐藏菜单栏
 (menu-bar-mode -1)
+
+;;隐藏^M
+(defun remove-dos-eol ()
+  "Do not show ^M in files containing mixed UNIX and DOS line endings."
+  (interactive)
+  (setq buffer-display-table (make-display-table))
+  (aset buffer-display-table ?\^M []))

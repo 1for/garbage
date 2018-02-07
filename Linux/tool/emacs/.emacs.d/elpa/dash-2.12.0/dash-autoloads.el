a5f5f40b8fbdbed109658846eb960a5c0e4d17e0
liuyang1-iri <liuyang1-iri@360.cn>
2018-02-07 16:11:03 +0800

    delete elpa

--- Linux/tool/emacs/.emacs.d/elpa/dash-2.12.0/dash-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("dash-functional.el" "dash-pkg.el" "dash.el")
-;;;;;;  (22396 54058 40631 52000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here

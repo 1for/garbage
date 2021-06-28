90f1eb181037ab418393b0b7cdd62acab8e7e31f
liuyang1-iri <liuyang1-iri@360.cn>
2021-06-28 12:51:17 +0800

    add leetcode-cn.com/bus-routes

---
+++ leetcode/leetcode-cn.com/bus-routes/main.go
@@ -0,0 +1,89 @@
+package main
+
+import(
+	"fmt"
+	"math"
+)
+
+func main(){
+	routes := [][]int{{1,2,7},{3,6,7}}
+	source := 1
+	target := 6
+
+	ret := numBusesToDestination(routes, source, target)
+	fmt.Println(ret)
+	
+}
+
+func numBusesToDestination(routes [][]int, source, target int) int {
+	if source == target {
+		return 0
+	}
+	
+	// 建立不同巴士线路的换乘关系
+	// exchange[fromRouteIdx][toRouteIdx]
+	numOfRoutes := len(routes)
+	exchange := make([][]bool, numOfRoutes)
+	for i := range exchange {
+		exchange[i] = make([]bool, numOfRoutes)
+	}
+	//初始化 exchange 完成
+
+	//记录站点所属的公交线路 rec[site] = [RouteIdx1,RouteIdx2]
+	
+	rec := make(map[int][]int)
+
+	//遍历公交线路
+	for routeIdx, route := range routes {
+		//遍历单个线路里面的站点
+		for _,site := range route {
+			//遍历站点所属的线路
+			for routeIdxOfSite := range rec[site] {
+				exchange[routeIdx][routeIdxOfSite] = true
+				exchange[routeIdxOfSite][routeIdx] = true
+			}
+			rec[site] = append(rec[site], routeIdx)
+		}
+	}
+
+	//distance 表示 source 到各个公交线路所搭乘的公交车数量
+	//distance[routeIdx] = numOfBus
+	distance := make([]int, numOfRoutes)
+	for i := range distance {
+		distance[i] = -1;
+	}
+	
+	//设置广度优先搜索的起始队列
+	queue := []int{}
+	for routeIdx := range rec[source] {
+		distance[routeIdx] = 1
+		queue = append(queue, routeIdx)
+	}
+
+	for len(queue) > 0 {
+		currentRoute := queue[0]
+		queue = queue[1:]
+
+		for toRoute, canExchange := range exchange[currentRoute] {
+			//可以换乘，并且线路之前未经过
+			if canExchange && distance[toRoute] == -1 {
+				distance[toRoute] = distance[currentRoute] + 1
+				queue = append(queue, toRoute)
+			}
+		}
+	}
+
+	ret := math.MaxInt32
+
+	for _,routeIdx := range rec[target]{
+		if distance[routeIdx] != -1 && distance[routeIdx] < ret {
+			ret = distance[routeIdx]
+		}
+	}
+
+	if ret < math.MaxInt32	{
+		return ret
+	}
+
+	return -1
+}
